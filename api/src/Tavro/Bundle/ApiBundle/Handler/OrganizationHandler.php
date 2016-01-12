148a6c59b86d316acb1f42faf2a153ba5044dfc5
Jake Litwicki <jake.litwicki@gmail.com>
2016-01-11 23:05:36 -0500

    add Organization handler

---
+++ api/src/Tavro/Bundle/ApiBundle/Handler/OrganizationHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tavro\Bundle\ApiBundle\Handler;
+
+use Tavro\Bundle\ApiBundle\Services\EntityHandler;
+
+/**
+ * Class OrganizationHandler
+ *
+ * @package Tavro\Bundle\ApiBundle\Handler
+ */
+class OrganizationHandler extends EntityHandler
+{
+
+}
---
+++ api/src/Tavro/Bundle/CoreBundle/Form/OrganizationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tavro\Bundle\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrganizationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+        ;
+    }
+    
+    /**
+     * Define the default validation groups.
+     *
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'validation_groups' => array('api'),
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'organization_type';
+    }
+}
