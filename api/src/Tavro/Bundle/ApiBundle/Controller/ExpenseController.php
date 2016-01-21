95e91883e23dfe2c12698a6202c73b3046e124f8
Jake Litwicki <jake.litwicki@gmail.com>
2016-01-21 00:06:42 -0500

    add expense api routes

---
+++ api/src/Tavro/Bundle/ApiBundle/Controller/ExpenseController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Tavro\Bundle\ApiBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Tavro\Bundle\ApiBundle\Exception\ApiException;
+use Tavro\Bundle\ApiBundle\Exception\ApiNotFoundException;
+use Tavro\Bundle\ApiBundle\Exception\ApiRequestLimitException;
+use Tavro\Bundle\ApiBundle\Exception\ApiAccessDeniedException;
+use Tavro\Bundle\CoreBundle\Exception\Form\InvalidFormException;
+
+use Doctrine\Common\Inflector\Inflector;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+use Tavro\Bundle\CoreBundle\Entity\Expense;
+use Tavro\Bundle\CoreBundle\Entity\ExpenseComment;
+use Symfony\Component\HttpFoundation\Cookie;
+
+use Litwicki\Common\Common;
+
+class ExpenseController extends ApiController
+{
+
+    /**
+     * Display all Comments for this Expense.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Tavro\Bundle\CoreBundle\Entity\Expense $expense
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function commentsAction(Request $request, Expense $expense, $_format)
+    {
+        try {
+
+            $entities = $expense->getExpenseComments();
+
+            $items = array();
+
+            foreach($entities as $entity) {
+                $items[] = $entity->getComment();
+            }
+
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+        finally {
+            $data = $this->serialize($items, $_format, $group = 'simple');
+            $response = $this->apiResponse($data, $_format);
+            return $response;
+        }
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Tavro\Bundle\CoreBundle\Entity\Expense $expense
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function newCommentAction(Request $request, Expense $expense, $_format)
+    {
+        try {
+
+            $data = json_decode($request->getContent(), true);
+
+            $handler = $this->getHandler('comment');
+            $comment = $handler->post($request, $data);
+
+            /**
+             * Attach the Comment to the Expense
+             */
+            $this->getHandler('expense_comment')->post($request, array(
+                'comment' => $comment->getId(),
+                'expense' => $expense->getId()
+            ));
+
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+        finally {
+            $routeOptions = array(
+                'entity'  => 'comment',
+                'id'      => $comment->getId(),
+                'format'  => $_format,
+            );
+
+            return $this->forward('TavroApiBundle:Default:get', $routeOptions);
+        }
+    }
+
+    /**
+     * Display all Tags for this Expense.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Tavro\Bundle\CoreBundle\Entity\Expense $expense
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function tagsAction(Request $request, Expense $expense, $_format)
+    {
+        try {
+
+            $entities = $expense->getExpenseTags();
+
+            $items = array();
+
+            foreach($entities as $entity) {
+                $items[] = $entity->getTag();
+            }
+
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+        finally {
+            $data = $this->serialize($items, $_format, $group = 'simple');
+            $response = $this->apiResponse($data, $_format);
+            return $response;
+        }
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Tavro\Bundle\CoreBundle\Entity\Expense $expense
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function newTagAction(Request $request, Expense $expense, $_format)
+    {
+        try {
+
+            $data = json_decode($request->getContent(), true);
+
+            $handler = $this->getHandler('tag');
+            $tag = $handler->post($request, $data);
+
+            /**
+             * Attach the Comment to the Expense
+             */
+            $this->getHandler('expense_tag')->post($request, array(
+                'comment' => $tag->getId(),
+                'expense' => $expense->getId()
+            ));
+
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+        finally {
+            $routeOptions = array(
+                'entity'  => 'comment',
+                'id'      => $tag->getId(),
+                'format'  => $_format,
+            );
+
+            return $this->forward('TavroApiBundle:Default:get', $routeOptions);
+        }
+    }
+
+}
