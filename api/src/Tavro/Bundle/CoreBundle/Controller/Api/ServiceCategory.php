16f51274bc94dd6e05b3fefd3a3bb9d332b02fe5
Jake Litwicki <jake.litwicki@gmail.com>
2016-03-01 22:53:56 -0500

    add category routes, add subroutes for primary objects

---
+++ api/src/Tavro/Bundle/CoreBundle/Controller/Api/ServiceCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tavro\Bundle\CoreBundle\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiException;
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiNotFoundException;
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiRequestLimitException;
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiAccessDeniedException;
+use Tavro\Bundle\CoreBundle\Exception\Form\InvalidFormException;
+
+use Doctrine\Common\Inflector\Inflector;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+use Tavro\Bundle\CoreBundle\Entity\Expense;
+use Tavro\Bundle\CoreBundle\Entity\ExpenseComment;
+use Symfony\Component\HttpFoundation\Cookie;
+
+use Litwicki\Common\Common;
+use Tavro\Bundle\CoreBundle\Controller\Api\DefaultController as ApiController;
+
+class ServiceCategoryController extends ApiController
+{
+
+}
