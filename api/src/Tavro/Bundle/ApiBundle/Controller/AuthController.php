4ae7dde07dda0f9ed2a63f86e4ba887f01c6a600
Jake Litwicki <jake.litwicki@gmail.com>
2016-08-08 12:02:19 -0700

    setup auth controller for api endpoints

---
+++ api/src/Tavro/Bundle/ApiBundle/Controller/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tavro\Bundle\ApiBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiException;
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiNotFoundException;
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiRequestLimitException;
+use Tavro\Bundle\CoreBundle\Exception\Api\ApiAccessDeniedException;
+use Tavro\Bundle\CoreBundle\Exception\Form\InvalidFormException;
+
+use Doctrine\Common\Inflector\Inflector;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+use Tavro\Bundle\CoreBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Cookie;
+
+use Litwicki\Common\Common;
+use Tavro\Bundle\ApiBundle\Controller\DefaultController as ApiController;
+
+class AuthController extends ApiController
+{
+
+}
