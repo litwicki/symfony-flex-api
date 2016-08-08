87e469bb1a97fb9ce14e44fdb25304ecfc62732b
Jake Litwicki <jake.litwicki@gmail.com>
2016-08-08 15:37:09 -0700

    add password forgot/reset

--- api/src/Tavro/Bundle/ApiBundle/Controller/AuthController.php
+++ api/src/Tavro/Bundle/ApiBundle/Controller/AuthController.php
@@ -24,4 +24,52 @@ use Tavro\Bundle\ApiBundle\Controller\DefaultController as ApiController;
 class AuthController extends ApiController
 {
 
+    /**
+     * By design, only allow the current user to do this for his/her self.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param $format
+     *
+     * @throws \Exception
+     */
+    public function resetAction(Request $request, $format)
+    {
+        try {
+            $handler = $this->container->get('tavro.handler.users');
+            $user = $this->getUser();
+            $data = json_decode($request->getContent(), true);
+            $handler->resetPassword($user, $data);
+        }
+        catch(ApiAccessDeniedException $e) {
+            throw $e;
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * When a User has forgotten their password, set a reset token and email forcing
+     * them to verify themselves before allowing the actual reset to complete.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param $format
+     *
+     * @throws \Exception
+     */
+    public function forgotAction(Request $request, $format)
+    {
+        try {
+            $handler = $this->container->get('tavro.handler.users');
+            $user = $this->getUser();
+            $handler->forgotPassword($user);
+        }
+        catch(ApiAccessDeniedException $e) {
+            throw $e;
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
 }
