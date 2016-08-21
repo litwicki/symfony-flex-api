e2df2ed02a61bff6d5700fbb827075f800f080f3
Jake Litwicki <jake.litwicki@gmail.com>
2016-08-21 16:38:03 -0700

    recreate service form

---
+++ api/src/Tavro/Bundle/CoreBundle/Form/ServiceType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tavro\Bundle\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ServiceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('body')
+            ->add('price')
+            ->add('type')
+            ->add('status')
+            ->add('category', EntityType::class, array(
+                'class' => 'TavroCoreBundle:ServiceCategory',
+                'choice_label' => 'Organization',
+                'invalid_message' => 'Please enter a valid Service Category'
+            ))
+            ->add('organization', EntityType::class, array(
+                'class' => 'TavroCoreBundle:Organization',
+                'choice_label' => 'Organization',
+                'invalid_message' => 'Please enter a valid Organization'
+            ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Tavro\Bundle\CoreBundle\Entity\Service',
+            'csrf_protection' => false
+        ));
+    }
+}
