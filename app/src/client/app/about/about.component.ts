6f9841452c24b60e216ab4aadbeb364b3f631817
Jake Litwicki <jake.litwicki@gmail.com>
2016-09-08 23:17:04 -0700

    setup angular2ts for app

---
+++ app/src/client/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded AboutComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'tavro-about',
+  templateUrl: 'about.component.html',
+  styleUrls: ['about.component.css']
+})
+export class AboutComponent { }
