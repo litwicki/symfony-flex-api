c211fff5c16445e29872cad2469eb05b303563b7
Jake Litwicki <jake.litwicki@gmail.com>
2016-10-30 14:56:05 -0700

    rebase data model

--- misc/scripts/reset-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-api_path=/var/www/tavro/api
-core_fixture_path=$api_path/src/Tavro/Bundle/CoreBundle/DataFixtures
-
-# Execute Migrations in a Symfony application
-sudo bash /var/www/tavro/scripts/reset-clean.sh
-sudo php $api_path/bin/console doctrine:fixtures:load --fixtures=$core_fixture_path/Demo --append
