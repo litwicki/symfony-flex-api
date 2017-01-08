0b7cf110e9deedc142fa07b826844fec7304d0e8
Jake Litwicki <jake.litwicki@gmail.com>
2017-01-07 16:32:51 -0800

    update namespaces on new api exception classes

---
+++ api/src/Tavro/Bundle/ApiBundle/Exception/ApiRequestSizeException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tavro\Bundle\ApiBundle\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * @author jake@zoadilack.com
+ */
+class ApiRequestSizeException extends HttpException
+{
+    /**
+     * Constructor.
+     *
+     * @param string $message      The internal exception message
+     * @param \Exception $previous The previous exception
+     * @param int $code            The internal exception code
+     */
+    public function __construct($message = NULL, \Exception $previous = NULL, $code = 400)
+    {
+        parent::__construct(500, $message, $previous, array(), $code);
+    }
+}
