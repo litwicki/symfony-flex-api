737b70d02f811c50838e641ff1cb89bcdd234b3e
Jake Litwicki <jake.litwicki@gmail.com>
2017-01-18 20:06:46 -0800

    update authenticator for lexik 2.1

--- api/src/Tavro/Bundle/ApiBundle/Security/Authentication/Authenticator/JwtAuthenticator.php
+++ api/src/Tavro/Bundle/ApiBundle/Security/Authentication/Authenticator/JwtAuthenticator.php
@@ -13,23 +13,43 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
 
 class JwtAuthenticator extends AbstractGuardAuthenticator
 {
     private $em;
     private $jwtEncoder;
 
+    /**
+     * JwtAuthenticator constructor.
+     *
+     * @param \Doctrine\ORM\EntityManager $em
+     * @param \Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoder $jwtEncoder
+     */
     public function __construct(EntityManager $em, JWTEncoder $jwtEncoder)
     {
         $this->em = $em;
         $this->jwtEncoder = $jwtEncoder;
     }
 
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\Security\Core\Exception\AuthenticationException|null $authException
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
     public function start(Request $request, AuthenticationException $authException = null)
     {
         return new JsonResponse('Auth header required', 401);
     }
 
+    /**
+     * Get the User Credentials from the Token.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return string|void
+     */
     public function getCredentials(Request $request)
     {
 
@@ -51,9 +71,25 @@ class JwtAuthenticator extends AbstractGuardAuthenticator
         return $token;
     }
 
+    /**
+     * Get the User Object from the Token.
+     *
+     * @param mixed $credentials
+     * @param \Symfony\Component\Security\Core\User\UserProviderInterface $userProvider
+     *
+     * @return null|object|void
+     */
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        $data = $this->jwtEncoder->decode($credentials);
+        try {
+            $data = $this->jwtEncoder->decode($credentials);
+        } catch (JWTDecodeFailureException $e) {
+            // if you want to, use can use $e->getReason() to find out which of the 3 possible things went wrong
+            // and tweak the message accordingly
+            // https://github.com/lexik/LexikJWTAuthenticationBundle/blob/05e15967f4dab94c8a75b275692d928a2fbf6d18/Exception/JWTDecodeFailureException.php
+
+            throw new CustomUserMessageAuthenticationException('Invalid Token');
+        }
 
         if(!$data) {
             return;
@@ -70,22 +106,49 @@ class JwtAuthenticator extends AbstractGuardAuthenticator
         return $user;
     }
 
-
+    /**
+     * @param mixed $credentials
+     * @param \Symfony\Component\Security\Core\User\UserInterface $user
+     *
+     * @return bool
+     */
     public function checkCredentials($credentials, UserInterface $user)
     {
         return TRUE;
     }
 
+    /**
+     * On Authentication Failure, return a response, increment, and log the failed attempt.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\Security\Core\Exception\AuthenticationException $exception
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         return new JsonResponse(['message' => $exception->getMessage()], 401);
     }
 
+    /**
+     * On success... do stuff
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\Security\Core\Authentication\Token\TokenInterface $token
+     * @param string $providerKey
+     *
+     * @return null|\Symfony\Component\HttpFoundation\Response|void
+     */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         return;
     }
 
+    /**
+     * Does this Method support Remember Me?
+     *
+     * @return bool
+     */
     public function supportsRememberMe()
     {
         return FALSE;
