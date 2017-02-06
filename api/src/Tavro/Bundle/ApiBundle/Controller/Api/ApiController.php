231e8698e9fbb5aef86b08ea43dd46ffbf941388
Jake Litwicki <jake.litwicki@gmail.com>
2017-02-06 15:14:30 -0800

    cleanup controllers for api actions

--- api/src/Tavro/Bundle/ApiBundle/Controller/Api/ApiController.php
+++ api/src/Tavro/Bundle/ApiBundle/Controller/Api/ApiController.php
@@ -18,84 +18,206 @@ use Tavro\Bundle\ApiBundle\Controller\DefaultController;
 class ApiController extends DefaultController
 {
     /**
-     * Get an Entity - forward to the appropriate controller to handle.
+     * Post (create) a new Entity
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param $entity
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function post(Request $request, $entity, $_format)
+    {
+        try {
+
+            $data = json_decode($request->getContent(), TRUE);
+            $handler = $this->getHandler($entity);
+            $newEntity = $handler->post($request, $data);
+
+            $routeOptions = array(
+                'entity'  => $entity,
+                'id'      => $newEntity->getId(),
+                'format'  => $_format,
+            );
+
+            return $this->forward('TavroApiBundle:Default:get', $routeOptions);
+        }
+        catch (InvalidFormException $e) {
+            throw $e;
+        }
+        catch (ApiAccessDeniedException $e) {
+            throw $e;
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * CREATE a new Entity if $id does not exist, otherwise PUT (update) existing Entity.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param $entity
+     * @param $id
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function putAction(Request $request, $entity, $id, $_format)
+    {
+        try {
+
+            $post = json_decode($request->getContent(), TRUE);
+
+            $handler = $this->getHandler($entity);
+
+            if (!($item = $handler->find($id))) {
+                $item = $handler->post($request, $item, $post);
+            }
+            else {
+                $item = $handler->put($request, $item, $post);
+            }
+
+            $routeOptions = array(
+                'entity'  => $entity,
+                'id'      => $item->getId(),
+                '_format'  => $_format,
+            );
+
+            return $this->forward('TavroApiBundle:Default:get', $routeOptions);
+        }
+        catch (InvalidFormException $e) {
+            throw $e;
+        }
+        catch(ApiAccessDeniedException $e) {
+            throw $e;
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Fetch a list of entities based on passed parameters that can be displayed
+     * in a typeahead autocomplete widget.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param $entity
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function typeaheadAction(Request $request, $entity, $_format)
+    {
+        try {
+            $params = $request->query->all();
+            $handler = $this->getHandler($entity);
+            $items = $handler->typeahead($params);
+            return $this->apiResponse($items, [
+                'format' => $_format,
+                'group' => 'typeahead'
+            ]);
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param $entity
+     * @param $_format
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function getAllAction(Request $request, $entity, $_format)
+    {
+        try {
+
+            $params = $request->query->all();
+            $handler = $this->getHandler($entity);
+            $items = $handler->findAll($params);
+            return $this->apiResponse($items, [
+                'format' => $_format
+            ]);
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Get (find) an entity by Id.
      *
      * @param $entity
      * @param $id
      * @param $_format
      *
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
     public function getAction($entity, $id, $_format)
     {
-        return $this->forward('TavroApiBundle:Api\Get:get', [
-            'entity'  => $entity,
-            'id' => $id,
-            '_format' => $_format
-        ]);
+        try {
+            $handler = $this->getHandler($entity);
+            $item = $handler->find($id);
+            return $this->apiResponse($item, [
+                'format' => $_format
+            ]);
+        }
+        catch(ApiAccessDeniedException $e) {
+            throw $e;
+        }
+        catch(\Exception $e) {
+            throw $e;
+        }
     }
 
     /**
-     * Get All Entities - forward to the appropriate handler.
-     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
      * @param $entity
      * @param $id
      * @param $_format
      *
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
-    public function getAllAction($entity, $id, $_format)
+    public function deleteAction(Request $request, $entity, $id, $_format)
     {
-        return $this->forward('TavroApiBundle:Api\Get:getAll', [
-            'entity'  => $entity,
-            'id' => $id,
-            '_format' => $_format
-        ]);
-    }
+        try {
 
-    /**
-     * PUT an Entity by redirecting to the PUT Controller.
-     *
-     * @param $entity
-     * @param $id
-     * @param $_format
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function putAction($entity, $id, $_format)
-    {
-        return $this->forward('TavroApiBundle:Api\Put:put', [
-            'entity'  => $entity,
-            'id' => $id,
-            '_format' => $_format
-        ]);
-    }
+            $handler = $this->getHandler($entity);
 
-    public function postAction($entity, $id, $_format)
-    {
-        return $this->forward('TavroApiBundle:Api\Post:post', [
-            'entity'  => $entity,
-            'id' => $id,
-            '_format' => $_format
-        ]);
-    }
+            $class = Inflector::singularize($entity);
+            $class = Inflector::classify($class);
 
-    /**
-     * PATCH an Entity by forwarding to the Patch Controller.
-     *
-     * @param $entity
-     * @param $id
-     * @param $_format
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function patchAction($entity, $id, $_format)
-    {
-        return $this->forward('TavroApiBundle:Api\Patch:patch', [
-            'entity'  => $entity,
-            'id' => $id,
-            '_format' => $_format
-        ]);
+            if ($data = $handler->find($id)) {
+                $handler->delete($request, $data);
+                $code = 200;
+                $message = sprintf('%s %s deleted.', $class, $id);
+            }
+            else {
+                $code = 404;
+                $message = sprintf('%s object not found.', $class);
+            }
+
+            return $this->apiResponse($data, [
+                'format' => $_format,
+                'code' => $code,
+                'message' => $message
+            ]);
+
+        }
+        catch (InvalidFormException $e) {
+            throw $e;
+        }
+        catch (\Exception $e) {
+            throw $e;
+        }
     }
 
 }
