69c6d07c4eef46ae90b8c6cf0bd4061162aac17f
Jake Litwicki <jake.litwicki@gmail.com>
2017-02-23 15:53:16 -0800

    stash

--- api/tests/ApiBundle/Security/Authentication/Authenticator/JwtAuthenticatorTest.php
+++ api/tests/ApiBundle/Security/Authentication/Authenticator/JwtAuthenticatorTest.php
@@ -5,15 +5,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Tests\Doctrine;
 use Tests\SymfonyKernel;
 
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Tavro\Bundle\CoreBundle\Entity\User;
+
 class JwtAuthenticatorTest extends \PHPUnit_Framework_TestCase
 {
     use SymfonyKernel;
     use Doctrine;
 
+    public function createToken($username = 'tavrobot')
+    {
+        return $this->container->get('lexik_jwt_authentication.encoder')->encode(['username' => $username]);
+    }
+
     public function testGetCredentials()
     {
 
-        $before = $this->container->get('lexik_jwt_authentication.encoder')->encode(['username' => 'tavrobot']);
+        $before = $this->createToken();
 
         $request = Request::createFromGlobals();
         $request->headers->set('Authorization', sprintf('Bearer %s', $before));
@@ -22,6 +32,28 @@ class JwtAuthenticatorTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($before, $after);
 
+    }
+
+    public function testGetUser()
+    {
+        $credentials = $this->createToken();
+        $data = $this->container->get('lexik_jwt_authentication.encoder')->decode($credentials);
+
+    }
+
+    public function testGetExpiredUser()
+    {
+        $credentials = $this->createToken('expired');
+    }
+
+    public function testInvalidUser()
+    {
+        $credentials = $this->createToken('invalid');
+    }
+
+    public function testUnverifiedUser()
+    {
+        $credentials = $this->createToken('unverified');
     }
 
 }
