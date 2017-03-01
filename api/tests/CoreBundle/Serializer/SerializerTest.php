802b0a12f6a7c940fd6428ee4cdf517f32194cb9
Jake Litwicki <jake.litwicki@gmail.com>
2017-02-28 22:01:07 -0800

    misc cleanup and updates for tests

---
+++ api/tests/CoreBundle/Serializer/SerializerTest.php
@@ -0,0 +1,46 @@
+<?php namespace Tests\CoreBundle\Serializer;
+
+use GuzzleHttp\Client;
+use Tavro\Bundle\CoreBundle\Entity\User;
+use Tavro\Bundle\CoreBundle\Tests\TavroCoreTest;
+use Tests\SymfonyKernel;
+
+class SerializerTest extends TavroCoreTest
+{
+    use SymfonyKernel;
+
+    public function testSerializeJson()
+    {
+        $user = new User();
+        $serializer = $this->container->get('tavro_serializer');
+        $json = $serializer->serialize($user, 'json');
+        $this->assertTrue((json_last_error() === JSON_ERROR_NONE));
+    }
+
+    public function testDeserializeJson()
+    {
+        $user = new User();
+        $serializer = $this->container->get('tavro_serializer');
+        $json = $serializer->serialize($user, 'json');
+        $entity = $serializer->deserialize($json, 'Tavro\CoreBundle\Entity\User', 'json');
+        $this->assertTrue(($entity instanceof User));
+    }
+
+    public function testSerializeXml()
+    {
+        $user = new User();
+        $serializer = $this->container->get('tavro_serializer');
+        $json = $serializer->serialize($user, 'xml');
+        $this->assertTrue((json_last_error() === JSON_ERROR_NONE));
+    }
+
+    public function testDeserializeXml()
+    {
+        $user = new User();
+        $serializer = $this->container->get('tavro_serializer');
+        $json = $serializer->serialize($user, 'xml');
+        $entity = $serializer->deserialize($json, 'Tavro\CoreBundle\Entity\User', 'json');
+        $this->assertTrue(($entity instanceof User));
+    }
+
+}
---
+++ api/tests/CoreBundle/TavroCoreTest.php
@@ -0,0 +1,8 @@
+<?php namespace Tavro\Bundle\CoreBundle\Tests;
+
+use GuzzleHttp\Client;
+
+class TavroCoreTest extends \PHPUnit_Framework_TestCase
+{
+
+}
