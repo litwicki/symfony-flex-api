9b3800cfe32d3d6e960d231791631cb3f2bf6156
Jake Litwicki <jake.litwicki@gmail.com>
2017-03-09 21:14:17 -0800

    cleanup

--- api/src/Tavro/Bundle/ApiBundle/Controller/AccountEntity/RevenueController.php
+++ api/src/Tavro/Bundle/ApiBundle/Controller/AccountEntity/RevenueController.php
@@ -71,34 +71,6 @@ class RevenueController extends AccountEntityApiController
     }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @param \Tavro\Bundle\CoreBundle\Entity\Account $account
-     * @param $_format
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
-     */
-    public function byAccountAction(Request $request, Account $account, $_format)
-    {
-        $data = null;
-
-        try {
-
-            $data = $account->getRevenues();
-
-            $options = [
-                'format' => $_format,
-                'group' => 'simple'
-            ];
-        }
-        catch(\Exception $e) {
-            $options = $this->getExceptionOptions($e, $_format);
-        }
-
-        return $this->apiResponse($data, $options);
-    }
-
-    /**
      * Display all Comments for this Expense.
      *
      * @param \Symfony\Component\HttpFoundation\Request $request
