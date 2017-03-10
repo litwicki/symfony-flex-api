64de7c5121dc30f673dc6a08f42b68f0d49987bc
Jake Litwicki <jake.litwicki@gmail.com>
2017-03-10 12:55:24 -0800

    add Jwt handler service wrapper

---
+++ api/src/Tavro/Bundle/ApiBundle/Security/Jwt/Jwt.php
@@ -0,0 +1,33 @@
+<?php namespace Tavro\Bundle\ApiBundle\Security\Jwt;
+
+use Tavro\Bundle\ApiBundle\Exception\ApiException;
+use Tavro\Bundle\CoreBundle\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\DefaultEncoder;
+
+class Jwt
+{
+    protected $encoder;
+
+    public function __construct(DefaultEncoder $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * Create a JWT Token.
+     *
+     * @param User $user
+     * @return string
+     */
+    public function createToken(User $user)
+    {
+        try {
+            return $this->encoder->encode(['username' => $user->getUsername()]);
+        }
+        catch(\Exception $e) {
+            throw new ApiException($e->getMessage());
+        }
+
+    }
+
+}
