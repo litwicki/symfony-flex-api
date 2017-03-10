f2d3a1528858093c423079cccfbca6fa806c25b5
Jake Litwicki <jake.litwicki@gmail.com>
2017-03-09 22:55:18 -0800

    update paths to fetching `all` entities

--- api/src/Tavro/Bundle/ApiBundle/Controller/AccountEntity/NodeController.php
+++ api/src/Tavro/Bundle/ApiBundle/Controller/AccountEntity/NodeController.php
@@ -250,32 +250,4 @@ class NodeController extends AccountEntityApiController
         return $this->apiResponse($data, $options);
     }
 
-    /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @param \Tavro\Bundle\CoreBundle\Entity\Account $account
-     * @param $_format
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
-     */
-    public function byAccountAction(Request $request, Account $account, $_format)
-    {
-        $data = null;
-
-        try {
-
-            $data = $account->getNodes();
-
-            $options = [
-                'format' => $_format,
-                'group' => 'simple'
-            ];
-        }
-        catch(\Exception $e) {
-            $options = $this->getExceptionOptions($e, $_format);
-        }
-
-        return $this->apiResponse($data, $options);
-    }
-
 }
