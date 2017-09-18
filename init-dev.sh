02654244e7b61ddca51f3bc52a0dca873ed22664
Jake Litwicki <jake.litwicki@gmail.com>
2017-09-18 11:56:27 -0700

    update init script to only create db if it doesnt exist add fixtures to init

--- init-dev.sh
+++ init-dev.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-cd /var/www/tavro/api && php bin/console doctrine:database:create
+# Create the database if it doesn't exist..
+cd /var/www/tavro/api && php bin/console doctrine:database:create --if-not-exists
+
+# Execute all migrations..
 cd /var/www/tavro/api && php bin/console doctrine:migrations:migrate
+
+# Execute fixtures for the environment
+# ** MAKE SURE TO INCLUDE --append OR YOU WILL LOSE EVERYTHING! **
+php bin/console doctrine:fixtures:load --fixtures=/var/www/tavro/api/vendor/zoadilack/tavro-core/Tavro/Bundle/CoreBundle/DataFixtures/Core --append
+
+# Do we want to include DEV test data?
+# php bin/console doctrine:fixtures:load --fixtures=/var/www/tavro/api/vendor/zoadilack/tavro-core/Tavro/Bundle/CoreBundle/DataFixtures/Dev --append
