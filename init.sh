5a6e96d33be56fea3bfbaebebda860e77e14089d
Jake Litwicki <jake.litwicki@gmail.com>
2017-10-12 15:11:20 -0700

    update default params file update init script

--- init.sh
+++ init.sh
@@ -1,41 +1,59 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-while [[ $# -gt 1 ]]
+for i in "$@"
 do
-key="$1"
+case $i in
+    -w=*|--webdir=*)
+    WEBROOT="${i#*=}"
 
-case $key in
-    -e|--env)
-    ENVIRONMENT="$2"
-    shift # past argument
     ;;
+    -e=*|--environment=*)
+    ENV="${i#*=}"
+    ;;
+
     --default)
     DEFAULT=YES
     ;;
+    
     *)
-    # unknown option
+    printf "\n=============================================================================\n\n"
+    printf "ERROR:\tOnly [-e|--environment] and [-w|--webdir] are valid arguments!"
+    printf "\n\n\texample: ./init.sh --env=dev --webdir=/var/www/tavro"
+    printf "\n\n=============================================================================\n\n"
+    exit;
     ;;
 esac
-shift # past argument or value
 done
 
-if [ -n "$ENVIRONMENT" ]; then
-    echo ENVIRONMENT  = "${ENVIRONMENT}"
-else
-    ENVIRONMENT="prod"
+# Assign default values
+APP_WEBROOT=${WEBROOT:=/var/www/tavro}
+APP_ENVIRONMENT=${ENV:=dev}
+
+if [[ ! ${APP_ENVIRONMENT} =~ "^[dev|test|prod]$" ]] ; then
+    printf "\n=============================================================================\n\n"
+    printf "ERROR:\tOnly acceptable environment values are: dev, test, prod"
+    printf "\n\n=============================================================================\n\n"
+    exit;
 fi
 
+echo APP_WEBROOT = ${APP_WEBROOT}
+echo APP_ENVIRONMENT = ${APP_ENVIRONMENT}
+
+exit;
+
 # Create the database if it doesn't exist..
-cd /var/www/tavro/api && php bin/console doctrine:database:create --if-not-exists
+php $WEBROOT/bin/console doctrine:database:create --if-not-exists
 
 # Execute all migrations..
-cd /var/www/tavro/api && php bin/console doctrine:migrations:migrate
+php $WEBROOT/bin/console doctrine:migrations:migrate
 
 # Execute fixtures for the environment
 # ** MAKE SURE TO INCLUDE --append OR YOU WILL LOSE EVERYTHING! **
-php bin/console doctrine:fixtures:load --fixtures=/var/www/tavro/api/vendor/zoadilack/tavro-core/Tavro/Bundle/CoreBundle/DataFixtures/Core --append
+php $WEBROOT/bin/console doctrine:fixtures:load --fixtures=$WEBROOT/vendor/zoadilack/tavro-core/Tavro/Bundle/CoreBundle/DataFixtures/Core --append
 
 # If we're in DEV, then also run the dev fixtures
 if [ "$ENVIRONMENT" = "dev" ]; then
-    php bin/console doctrine:fixtures:load --fixtures=/var/www/tavro/api/vendor/zoadilack/tavro-core/Tavro/Bundle/CoreBundle/DataFixtures/Dev --append
+    php $WEBROOT/bin/console doctrine:fixtures:load --fixtures=$WEBROOT/vendor/zoadilack/tavro-core/Tavro/Bundle/CoreBundle/DataFixtures/Dev --append
 fi
+
+php $WEBROOT/bin/console cache:clear --env=${APP_ENVIRONMENT} --no-warmup
