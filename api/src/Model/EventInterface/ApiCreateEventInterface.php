b44a345e1ef7ff83ca762fd3ab0b8110af1b60db
Jake Litwicki <jake.litwicki@gmail.com>
2018-03-06 10:43:45 -0800

    remove vagrantfile

---
+++ api/src/Model/EventInterface/ApiCreateEventInterface.php
@@ -0,0 +1,13 @@
+<?php namespace App\Model\EventInterface;
+
+use App\Model\EntityInterface\EntityInterface;
+
+/**
+ * Interface EntityInterface
+ *
+ * @package Api\Model\EventInterface
+ */
+interface ApiCreateEventInterface extends EntityInterface
+{
+
+}
---
+++ api/src/Model/EventInterface/ApiDeleteEventInterface.php
@@ -0,0 +1,13 @@
+<?php namespace App\Model\EventInterface;
+
+use App\Model\EntityInterface\EntityInterface;
+
+/**
+ * Interface EntityInterface
+ *
+ * @package Api\Model
+ */
+interface ApiDeleteEventInterface extends EntityInterface
+{
+
+}
